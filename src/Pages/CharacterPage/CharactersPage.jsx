180440e8a1e992b65719385a88bc77e98776cd5c
Lhamia <lamia.romero@bootcamp-upgrade.com>
2022-04-08 21:45:08 +0200

    cambio en mi rama

--- src/Pages/CharacterPage/CharactersPage.jsx
+++ src/Pages/CharacterPage/CharactersPage.jsx
@@ -1,38 +1,66 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
-import Card from "../../Components/Card/Card";
-import "./character.css"
-import Search from "../../Components/Search/Search";
-
-export default function CharactersPage(){
-    const [characters, setCharacters]= useState([])
-
-    const getCharacters = async (characterName = "")=>{
-      const res = await axios.get(`https://api.got.show/api/show/characters/${characterName}`)
-
-      console.log(res.data)
-      setCharacters(res.data)
-
-
-  }
-    useEffect(()=>{
-       
-        getCharacters()
-    },[])
-
-    
-  return (
-     <section>
-       <div className="b-Buscador">
-        <Search onSubmit={ (data) =>getCharacters ("character/:name=" + data.name)}/>
-     </div>
-    <div className="c-gallery">
+import { useEffect, useState } from "react"; 
+import axios from "axios"; 
+import Card from "../../Components/Card/Card"; 
+import "./character.css" 
+ 
+ 
+export default function CharactersPage(){ 
+    const [characters, setCharacters]= useState([]) 
+    const [charfiltered, setCharFiltered] = useState([]) 
+    const [search, setSearch] = useState("") 
+ 
+    const handleSearch = (ev)=>{ 
+      setSearch (ev.target.value) 
+      console.log(ev.target.value) 
+    } 
+ 
+  const filtrar = ()=>{ 
+    if ( search !=""){ 
+      const filtrados = characters.filter((character)=> character.name.toLowerCase().includes(search.toLowerCase())) 
+    setCharFiltered(filtrados); 
+ 
+    } 
      
-      {characters.map((character) => (
-        <Card character={character} />
-      ))}
-    </div></section>
-    
-  )
-
+     
+     
+  } 
+ 
+    const getCharacters = async ()=>{ 
+      const res = await axios.get(`https://api.got.show/api/show/characters/`) 
+ 
+      console.log(res.data) 
+      setCharacters(res.data) 
+      setCharFiltered(res.data) 
+ 
+ 
+  } 
+    useEffect(()=>{ 
+        
+        getCharacters() 
+    },[]) 
+ 
+    useEffect(()=>{ 
+        
+      filtrar() 
+  },[search]) 
+ 
+     
+  return ( 
+     <section> 
+       <div className="b-Buscador"> 
+       <input placeholder={"search.."}className="input" type="text" onChange={handleSearch}/>  
+ 
+         
+ 
+     
+     </div> 
+    <div className="c-gallery"> 
+      
+      {charfiltered.map((character) => ( 
+        <Card character={character} /> 
+      ))} 
+    </div></section> 
+     
+  ) 
+ 
 }
